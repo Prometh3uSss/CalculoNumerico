59ec35a3b204ced3dae0e823526cdf984cd2bcb0
Prometh3uSss <romardavid.4126@gmail.com>
2025-01-22 21:12:45 -0400

    Modificado el proyecto

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,13 +1,4 @@
-class Persona:
-    def __init__(self, nombre, edad):
-        self.nombre = nombre
-        self.edad = edad
+print("Hello Word")
 
-    def comparaEdad(self):
-
-
-p1 = Persona('Samuel', 24)
-p2 = Persona('Jael', 36)
-p3 = Persona('Liliana', 24)
 
 
